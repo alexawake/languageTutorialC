7b942ae3cf297b2bb7ba73905ed0ad2e182f9c47
Alejandro Xalabarder <appdev101@wakeupthecat.com>
2019-05-26 21:26:28 +0200

    Update PaulaS.c
    + Fix checks of indexes
    + Intialization of feld_offen
    + function show_offen
    
    working

--- exercises/labyrinth/PaulaS.c
+++ exercises/labyrinth/PaulaS.c
@@ -1,68 +1,90 @@
 #include <stdio.h>
 
-int feld[4][5] =
+#define MAXV 4
+#define MAXH 5
+
+int feld[MAXV][MAXH] =
 {
-   { 0, 0, 0, 1, 0 },
-   { 1, 0, 0, 1, 0 },
-   { 0, 1, 0, 1, 0 },
-   { 0, 1, 0, 0, 0 }
+    { 0, 0, 0, 1, 0 },
+    { 1, 0, 0, 1, 0 },
+    { 0, 1, 0, 1, 0 },
+    { 0, 1, 0, 0, 0 }
 };
 
-int feld_offen[4][5] = { 2 };
+int feld_offen[MAXV][MAXH] = 
+{
+    { 2, 2, 2, 2, 2 },
+    { 2, 2, 2, 2, 2 },
+    { 2, 2, 2, 2, 2 },
+    { 2, 2, 2, 2, 2 }
+};
 
 //momentaner Standort
 int v = 0;
 int h = 0;
 
-void schritt ()
+void show_offen()
+{
+    for (int vv = 0; vv < MAXV; vv++)
+    {
+        printf("\n");
+        for (int hh = 0; hh < MAXH; hh++)
+            printf(" %2d", feld_offen[vv][hh]);
+    }
+    printf("\n");
+}
+
+void schritt()
 {
    int hn = h, vn = v;
    int eingabe;
 
    //Schritteingabe mit dem Nummernfeld
-   printf("Geben Sie ihren nächsten Schritt ein");
+   printf("Geben Sie ihren nächsten Schritt ein (6, 4, 2 or 8): ");
    scanf("%d", &eingabe);
    switch (eingabe)
-       {
-      case 6: hn = h + 1; break;
-      case 4: hn = h - 1; break;
-      case 2: vn = v - 1; break;
-      case 8: vn = v + 1; break;
-      default: {
-             printf("Ungültige Eingabe\n");
-             return;
-            }
-       }
-
-   //Prüfen, ob Schritt möglich ist
-   if (hn < 0 || hn > 4 || vn < 0 || vn > 5)
    {
-      printf("Sie dürfen das Spielfeld nicht verlassen");
-   }
+    case 6: hn = h + 1; break;
+    case 4: hn = h - 1; break;
+    case 2: vn = v - 1; break;
+    case 8: vn = v + 1; break;
+    default: {
+              printf("\nUngültige Eingabe!\n");
+              return;
+             }
+    }
 
-   //Prüfen, ob Weg richtig ist
-   if (feld[vn][hn] == 0)
-   {
-      h = hn;
-      v = vn;
-      printf("Sie sind ein Feld weiter gelaufen");
-      feld_offen[vn][hn] = 0;
-      //printf("%d", feld_offen);
-   }
+    //Prüfen, ob Schritt möglich ist
+    if (hn < 0 || hn >= MAXH || vn < 0 || vn >= MAXV)
+    {
+        printf("Sie dürfen das Spielfeld nicht verlassen\n");
+        return;
+    }
 
-   if (feld[vn][hn] == 1)
-   {
-      h = hn;
-      v = vn;
-      printf("Dieser Schritt war falsch");
-      feld_offen[vn][hn] = 1;
-      //printf("%d", feld_offen);
-   }
+    //Prüfen, ob Weg richtig ist
+    if (feld[vn][hn] == 0)
+    {
+        h = hn;
+        v = vn;
+        printf("Sie sind ein Feld weiter gelaufen\n");
+        feld_offen[vn][hn] = 0;
+        show_offen();
+        return;
+    }
+
+    if (feld[vn][hn] == 1)
+    {
+        printf("Dieser Schritt war falsch\n");
+        feld_offen[vn][hn] = 1;
+        show_offen();
+        return;
+    }
 }
+
 
 int main()
 {
-   while (1)
-      schritt ();
-   return 0;
+    while (1)
+        schritt();
+    return 0;
 }
