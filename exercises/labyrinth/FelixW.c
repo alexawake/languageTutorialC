386a74792fcde68d1ccddce72f28990199ca6b74
Alejandro Xalabarder <appdev101@wakeupthecat.com>
2019-05-26 21:50:09 +0200

    Update FelixW.c
    Fixed and add function show

--- exercises/labyrinth/FelixW.c
+++ exercises/labyrinth/FelixW.c
@@ -1,47 +1,68 @@
 #include <stdio.h>
 
+#define bool char
+#define true 1
+#define false 0
+
 int feld[6][4] = { 0 };
-feld[0][1] = 1;
-feld[4][0] = 1;
-feld[4][1] = 1;
-feld[4][2] = 1;
-feld[2][1] = 1;
-feld[2][2] = 1;
-feld[3][1] = 1;
 
 bool step(int i, int j)
 {
-   if (feld[i][j]  )
+   if (i < 0 || j < 0 || i >= 6 || j >= 4) return false;
 
    if (i == 0 && j == 0)
    {
+      feld[i][j] = 9;
       return true;
    }
-   if (feld[i][j] == 1)
+   if (feld[i][j] != 0)
    {
       return false;
    }
+
+   feld[i][j] = 2;
+
    if (step(i, j + 1))
    {
-      printf("step(i, j+1)")
+      return true;
    }
    if (step(i, j - 1))
    {
-
+      return true;
    }
    if (step(i + 1, j))
    {
-
+      return true;
    }
    if (step(i - 1, j))
    {
+      return true;
+   }
 
+   return false;
+}
+
+void show ()
+{
+   for (int i = 0; i < 6; i ++)
+   {
+      printf ("\n");
+      for (int j = 0; j < 4; j ++)
+         printf (" %2d", feld[i][j]);         
    }
 }
 
 int main()
 {
-   step(0, 5);
+   feld[0][1] = 1;
+   feld[4][0] = 1;
+   feld[4][1] = 1;
+   feld[4][2] = 1;
+   feld[2][1] = 1;
+   feld[2][2] = 1;
+   feld[3][1] = 1;
+   step(0, 3);
+   show ();
 
-   system("pause");
+   // system("pause");
 }
