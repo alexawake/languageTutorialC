af82dbfdf4021c7012399b1f021df421d096cf28
Alejandro Xalabarder <appdev101@wakeupthecat.com>
2019-05-26 20:29:59 +0200

    Update MaxB.c
    - Fix mark cell to avoid endless loops
    - Fix if solve returns 1 we have to return as well, only in the case of cul-de-sac return 0
    - Show result
    
    working now

--- exercises/labyrinth/MaxB.c
+++ exercises/labyrinth/MaxB.c
@@ -3,71 +3,65 @@
 
 int solve(int field[7][7], int x, int y)
 {
-    if(field[x][y] == 4) //4 markiert Ziel, wenn dieses erreicht wird, Rückgabe 1
+    if (field[x][y] == 4) //4 markiert Ziel, wenn dieses erreicht wird, Rückgabe 1
     {
         return 1;
     }
-    else if(field[x+1][y] == 0)
+
+    // mark cell!
+    field[x][y] = 2;
+
+    if (field[x + 1][y] == 0)
     {
-        if(solve(field, x+1, y) == 1)
+        if (solve(field, x + 1, y) == 1)
         {
             printf("rechts");
-        }
-        else
-        {
-            x--;
+            return 1;
         }
     }
-    else if(field[x][y+1] == 0)
+
+    if (field[x][y + 1] == 0)
     {
-        if(solve(field, x, y+1) == 1)
+        if (solve(field, x, y + 1) == 1)
         {
             printf("hoch");
-        }
-        else
-        {
-            y--;
+            return 1;
         }
     }
-    else if(field[x-1][y] == 0)
+
+    if (field[x - 1][y] == 0)
     {
-        if(solve(field, x-1, y) == 1)
+        if (solve(field, x - 1, y) == 1)
         {
             printf("links");
-        }
-        else
-        {
-            x++;
+            return 1;
         }
     }
-    else if(field[x][y-1] == 0)
+
+    if (field[x][y - 1] == 0)
     {
-        if(solve(field, x, y-1) == 1)
+        if (solve(field, x, y - 1) == 1)
         {
             printf("runter");
-        }
-        else
-        {
-            y++;
+            return 1;
         }
     }
-    
+
     return 0; //wenn Funktion hier angelangt, ist Weg nicht richtig
-    
-    
 }
 
 int main()
 {
-    int field[7][7] =   {1, 1, 1, 1, 1, 1, 1,  //Mauer aus einsen um Labyrinth
-                         1, 0, 0, 1, 1, 4, 1, 
-                         1, 1, 0, 0, 1, 0, 1, 
-                         1, 1, 1, 0, 1, 0, 1, 
-                         1, 1, 1, 0, 1, 0, 1,
-                         1, 0, 1, 0, 0, 0, 1,
-                         1, 1, 1, 1, 1, 1, 1};
-    
-    if(solve(field, 1, 1) == 0)
+    int field[7][7] = { 
+        1, 1, 1, 1, 1, 1, 1,  //Mauer aus einsen um Labyrinth
+        1, 0, 0, 1, 1, 4, 1,
+        1, 1, 0, 0, 1, 0, 1,
+        1, 0, 1, 0, 1, 0, 1,
+        1, 0, 1, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1 };
+
+    if (solve(field, 1, 1) == 0)
     {
         printf("Fertig :)");
     }
@@ -75,12 +69,12 @@ int main()
     {
         printf("Es gibt keinen Weg");
     }
-    /*for(int i = 0, i <= 7, i++)
-        {
-            for(int j = 0, j <= 7, j++) 
-                {
-                    
-                }
-        }*/
-    
+    for(int i = 0; i < 7; i++)
+    {
+      printf("\n");
+      for(int j = 0; j < 7; j++)
+      {
+        printf(" %2d", field[i][j]);
+      }
+    }
 }
