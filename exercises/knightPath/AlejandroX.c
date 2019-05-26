f741af133a56655c15db2693a505d2ed7f18b0e9
Alejandro Xalabarder <appdev101@wakeupthecat.com>
2019-05-26 19:01:52 +0200

    Create AlejandroX.c
    - The algorithm is very similar to the one for labyrinth problem (labyrinth/AlejandroX.c)
    - Note for the count of backward movements that although the procedure is expressed in few lines
      thanks to the recursion it is indeed a brute force approach
    - The order of the movements when trying to do the next jump affects the performance of the algorithm a lot

---
+++ exercises/knightPath/AlejandroX.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+
+#define bool char
+#define false 0
+#define true 1
+
+#define DIM 8
+
+int field [DIM][DIM] = { 0 };
+int failures = 0;
+
+bool legalmove (int i, int k)
+{
+   if (i < 0 || k < 0) return false;
+   if (i >= DIM || k >= DIM) return false;
+   return field[i][k] == 0;
+}
+
+bool jump (int i, int k, int nmove)
+{
+   if (!legalmove (i, k)) return false;
+
+   field[i][k] = nmove;
+
+   // Check if finished (8 * 8 = 64 movement)
+   //
+   if (nmove >= DIM * DIM) 
+      return true;
+   
+   //printf ("do jump %d, %d\n", i, k);
+   
+   // try all the possible ways recursively
+   //
+   if (jump(i-1, k-2, nmove+1) ||
+       jump(i-2, k-1, nmove+1) ||
+       jump(i-2, k+1, nmove+1) ||
+       jump(i-1, k+2, nmove+1) ||
+       jump(i+1, k+2, nmove+1) ||
+       jump(i+2, k+1, nmove+1) ||
+       jump(i+2, k-1, nmove+1) ||
+       jump(i+1, k-2, nmove+1)
+      )
+   {
+       return true;
+   }
+
+   // unmark it for other possible path
+   field[i][k] = 0;
+   failures ++;
+
+   return false;
+}
+
+int main (int nn, char ** aa)
+{
+   if (jump (0, 0, 1))
+   {
+      printf ("solution found with %.3f thousand backward moves!\n", (float) (failures / 1000.f));
+   }
+   else
+   {
+      printf ("no solution found!\n");
+   }
+   for (int ii = 0; ii < DIM; ii ++)
+   {
+      for (int kk = 0; kk < DIM; kk ++)
+         printf (" %2d", field[ii][kk]);
+      printf ("\n");
+   }
+}
+
+
+/*
+
+----------- try using
+
+   if (jump(i-1, k-2, nmove+1) ||
+       jump(i-2, k-1, nmove+1) ||
+       jump(i-2, k+1, nmove+1) ||
+       jump(i-1, k+2, nmove+1) ||
+       jump(i+1, k+2, nmove+1) ||
+       jump(i+2, k+1, nmove+1) ||
+       jump(i+2, k-1, nmove+1) ||
+       jump(i+1, k-2, nmove+1))
+
+solution found with 251825.344 thousand backward moves!
+  1  8 11 16  3 18 13 64
+ 10 27  2  7 12 15  4 19
+ 53 24  9 28 17  6 63 14
+ 26 39 52 23 62 29 20  5
+ 43 54 25 38 51 22 33 30
+ 40 57 42 61 32 35 48 21
+ 55 44 59 50 37 46 31 34
+ 58 41 56 45 60 49 36 47
+ 
+----------- try using
+
+   if (jump(i-1, k-2, nmove+1) ||
+       jump(i-1, k+2, nmove+1) ||
+       jump(i+1, k+2, nmove+1) ||
+       jump(i-2, k+1, nmove+1) ||
+       jump(i-2, k-1, nmove+1) ||
+       jump(i+2, k+1, nmove+1) ||
+       jump(i+2, k-1, nmove+1) ||
+       jump(i+1, k-2, nmove+1)
+       
+       
+   too much computation, will it stop ... ?
+
+*/
