ee4779517e2139cdea65c7c09681a098f839f749
Alejandro Xalabarder <appdev101@wakeupthecat.com>
2019-05-26 18:43:21 +0200

    Update DavidF.c
    + do proper validity checks
    + mark cell when traveled and unmark it in case of cul-de-sac (no exit found)
    + add showlab function to output solution
    solution working

--- exercises/labyrinth/DavidF.c
+++ exercises/labyrinth/DavidF.c
@@ -2,18 +2,28 @@
 #include <stdlib.h>
 int lab [5][5] ={   0,0,0,1,0,
                     1,1,0,1,0,
-                    0,1,0,1,0,
-                    0,1,0,0,0,
-                    1,1,1,1,1};
+                    0,0,0,1,0,
+                    0,0,0,0,0,
+                    1,0,1,0,1};
 
-int step(int n, int k){
+int step(int n, int k)
+{
+    if(n>=5 || k>=5){       // Prüfen, ob außerhab des Feldes
+        return 1;
+    }
     if(n<0 || k<0){                 // Prüfen, ob außerhab des Feldes
+        return 1;
+    }
+    if(lab[n][k] != 0){             // Prüfen, ob Feld nicht erlaubt ist
         return 1;
     }
 
     if(n==0 && k==0){               //Endposition erreicht. Ziel erfüllt!
+        lab[n][k] = 9;
         return 0;
     }
+
+    lab[n][k] = 2;   // markieren als Pfad Versucht
 
     if(step(n,k-1) == 0){           //Links prüfen ob frei ist
         return 0;
@@ -31,9 +41,25 @@ int step(int n, int k){
         return 0;
     }
 
+    lab[n][k] = 3;   // markieren als kein Erfolg!
+    return 1;
+}
+
+void showlab ()
+{
+   int nn;
+   int kk;
+   for (nn = 0; nn < 5; nn ++)
+   {
+      printf ("\n");
+      for (kk = 0; kk < 5; kk ++)
+         printf (" %2d", lab[nn][kk]);
+   }
+   printf ("\n");
 }
 
 int main()
 {
-    step(0, 4);                     //Startposition wird übergeben
+   step(0, 4);                     //Startposition wird übergeben
+   showlab ();
 }
